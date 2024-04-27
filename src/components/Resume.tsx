8dca85839a0096a98d31a3c152a005e754870f1f
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-27 01:47:07 -0500

    refactor: :recycle: refactor resume section

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,3 +1,34 @@
+const ResumeItems = [
+  {
+    id: 1,
+    value: 0,
+    label: "Consumidas"
+  },
+  {
+    id: 2,
+    value: 0,
+    label: "Ejercicio"
+  },
+  {
+    id: 3,
+    value: 20,
+    label: "Diferencia"
+  }
+]
+
+function ResumeItem ({label, value}) {
+  return (
+    <div className="flex flex-col justify-center gap-2">
+      <p className="text-6xl text-white font-semibold text-center">
+        {value}
+      </p>
+      <p className="text-sm text-white font-semibold text-center">
+        {label}
+      </p>
+    </div>
+  )
+}
+
 export default function Resume () {
   return (
     <section className="bg-slate-800 py-10">
@@ -8,32 +39,15 @@ export default function Resume () {
 
       <div className="flex flex-col justify-between px-4 gap-4
       md:flex-row">
-        <div className="flex flex-col justify-center gap-2">
-          <p className="text-6xl text-white font-semibold text-center">
-            0
-          </p>
-          <p className="text-sm text-white font-semibold text-center">
-            Consumidas
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center gap-2">
-          <p className="text-6xl text-white font-semibold text-center">
-            0
-          </p>
-          <p className="text-sm text-white font-semibold text-center">
-            Ejercicio
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center gap-2">
-          <p className="text-6xl text-white font-semibold text-center">
-            0
-          </p>
-          <p className="text-sm text-white font-semibold text-center">
-            Diferencia
-          </p>
-        </div>
+        {
+          ResumeItems.map((item) => (
+            <ResumeItem
+              key={item.id}
+              label={item.label}
+              value={item.value}
+            />
+          ))
+        }
       </div>
     </div>
     </section>
